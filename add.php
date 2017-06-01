7b982f69d75afa95ff26de0e09248b7def2f209d
Paul Tonning <paul@dotclientsolutions.com>
2017-06-01 08:00:06 -0700

    Refactor

--- add.php
+++ add.php
@@ -7,3 +7,27 @@ $page->title = 'Add Task';
 include 'header.php';
 include 'form_tpl.php';
 include 'footer.php';
+
+// 1.
+// Put the Task class in a file called Task.php
+// Then include it here as is done for the header.php and form_tpl.php and footer.php 
+// files above
+
+function addTask($input) {
+     // 2.
+     // Put code here to add the task to the CSV file
+    // you should use the Task class that you developed earlier here
+    // the following function is only called right now
+    // to "debug" the addTask function, it should not be in the final
+    // version of this code
+    var_dump($input);
+    
+}
+
+// 3. 
+// this check is what will start the process to add the task , it assures
+// the process of not printing anything other than the form and a success message
+// when the task is successfully added.
+if(isset($_POST['submit'])) {
+   addTask($_POST); 
+}
--- list.php
+++ list.php
@@ -4,4 +4,30 @@ include 'Page.php';
 $page = new Page();
 $page->title = 'List';
 include 'header.php';
+
+// Add code here that will load the CSV file with the tasks in it
+// and will print the tasks in a table
+// The following is an example of how the table will be printed 
+// between the header and the footer
+echo '<table class="table">';  // this is styled as a bootstrap table
+echo '    <thead>';
+echo '      <tr>';
+echo '        <th>Task</th>';
+echo '        <th>Description</th>';
+echo '        <th>Completed(Y/N)</th>';
+echo '        <th>Date Completed</th>';
+echo '      </tr>';
+echo '    </thead>';
+echo '    <tbody>';
+echo '      <tr>';
+echo '        <td>The Task</td>';
+echo '        <td>The Task Description</td>';
+echo '        <td>Y</td>';
+echo '        <td>6/1/2018</td>';
+echo '      </tr>';
+echo '    </tbody>';
+echo '</table>';
+
+
 include 'footer.php';
+
